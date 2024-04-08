51b0288ab098119701c46f36df34c43e57b30577
romerodav <romerodav@hotmail.com>
2024-04-07 23:41:12 -0300

    Finishing

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,6 @@
 let year = new Date().getFullYear();
 document.querySelector("footer p").append(year);
+
+document.getElementById("lastModified").innerHTML = "Last Modification " + document.lastModified;
+
+/*document.getElementById("lastModified").textContent = "any text";*/
